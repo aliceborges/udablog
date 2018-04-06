d09ff7d61579d6fe36771cda0890cf6b5e6dd2a4
Alice Borges <aliceborges@outlook.com.br>
2018-04-06 13:37:54 -0300

    feat: colocando título na página

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+document.title = 'Udablog - Blog da Udacity';
+
 ReactDOM.render(<App />, document.getElementById('root'));
