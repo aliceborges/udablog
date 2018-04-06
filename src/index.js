ba597b817d548bd8fa9bfa348af060465e39a037
Alice Borges <aliceborges@outlook.com.br>
2018-04-06 19:11:57 -0300

    refactor: retirando importação não necessária

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { AddPost } from './actions';
 import reducer from './reducers';
 
 document.title = 'Udablog - Blog da Udacity';
