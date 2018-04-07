a00e022d0e2474628c20c77456db9a83baa2cfb8
Alice Borges <aliceborges@outlook.com.br>
2018-04-06 22:19:16 -0300

    feat: criação de função

---
+++ src/util/helper.js
@@ -0,0 +1,10 @@
+export const searchId = (array, id) => {
+  return array.findIndex((element, index) => {
+    if (element.id === id){
+      return index;
+    }
+    else{
+      return false;
+    }
+  });
+}
