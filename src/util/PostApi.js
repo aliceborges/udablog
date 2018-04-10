663faed6c2e6eabb21c8764874f49c9f29440201
Alice Borges <aliceborges@outlook.com.br>
2018-04-10 10:55:11 -0300

    feat: adição do CRUD na API de post

--- src/util/PostApi.js
+++ src/util/PostApi.js
@@ -5,4 +5,32 @@ const headers = {
   'Authorization': 'anything'
 };
 
-export const getAll = () => fetch(api, {headers}).then(res=>res.json());
+export const getAll = () =>
+  fetch(api, {headers}).
+  then(res=>res.json());
+
+export const addPost = (post) =>
+  fetch(api, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  }).then(res => res.json());
+
+export const edit = (id, post) =>
+  fetch(api + '/' + id, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  }).then(res => res.json());
+
+export const remove = (id) =>
+  fetch(api + '/' + id,{
+    method: 'DELETE',
+    headers
+  }).then(res => res.json());
