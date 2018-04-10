c92ae8b80ed34361e999cb9cf2b25ba9f8455edb
Alice Borges <aliceborges@outlook.com.br>
2018-04-10 10:46:59 -0300

    feat: conectando as APIs ao projeto e colocando a opção de adicionar categoria e post na tela inicial

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import { Navbar, Grid } from 'react-bootstrap';
 import ListPosts from './Post/List';
 import { connect } from 'react-redux';
+import * as CategoriesApi from '../util/CategoriesApi';
+import * as PostApi from '../util/PostApi';
+import { addCategory, addPost } from '../actions';
 
 import '../styles/css/bootstrap.min.css';
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addCategory: category => dispatch(addCategory(category)),
+    addPost: post => dispatch(addPost(post))
+  };
+};
+
 class App extends Component {
+
+  componentDidMount(){
+    CategoriesApi.getAll().then((categories) => {
+      categories.map(category => {
+        this.props.addCategory(category);
+        return category;
+      });
+    });
+
+    PostApi.getAll().then((posts)=>{
+      posts.map(post => {
+        this.props.addPost(post);
+        return post;
+      });
+    });
+  }
+
   render() {
     return (
       <div>
@@ -24,4 +51,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
