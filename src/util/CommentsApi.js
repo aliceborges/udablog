058c56acde629e192258c0f4e04cc9ab0a9707f0
Alice Borges <aliceborges@outlook.com.br>
2018-04-11 08:55:31 -0300

    feat: adição de mais funções na API de comentários

--- src/util/CommentsApi.js
+++ src/util/CommentsApi.js
@@ -1,4 +1,5 @@
 const api = 'http://localhost:3001/posts';
+const apiComment = 'http://localhost:3001/comments';
 
 const headers = {
   'Accept': 'application/json',
@@ -6,3 +7,39 @@ const headers = {
 };
 
 export const getAll = (idPost) => fetch(api + '/' + idPost + '/comments', {headers}).then(res=>res.json());
+
+export const add = (comment) =>
+fetch(apiComment,{
+	method: 'POST',
+	headers: {
+	  ...headers,
+	  'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(comment)
+}).then(res => res.json());
+
+export const edit = (id, comment) =>
+fetch(apiComment +'/'+ id, {
+	method: 'PUT',
+	headers: {
+	  ...headers,
+	  'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(comment)
+}).then(res => res.json());
+
+export const remove = (id) =>
+fetch(apiComment +'/'+ id, {
+	method: 'DELETE',
+	headers
+}).then(res => res.json());
+
+export const voted = (id, option) =>
+fetch(apiComment +'/'+ id,{
+	method: 'POST',
+	headers: {
+	  ...headers,
+	  'Content-Type': 'application/json'
+	},
+	body: JSON.stringify({ option })
+}).then(res => res.json());
